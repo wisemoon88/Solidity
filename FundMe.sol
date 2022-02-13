b98f2490c77a0f2c92504443b54d3c97f39ceec5
QAMARUL ARIFIN BIN ABD MANAN <78649201+wisemoon88@users.noreply.github.com>
2022-02-13 15:52:07 +0800

    Update FundMe.sol
    updating FundMe.Sol to complete the application

--- FundMe.sol
+++ FundMe.sol
@@ -13,14 +13,21 @@ contract FundMe{
 
     //uint256 public variable;
     mapping(address => uint256) public addressToFund;
+    address public owner;
+    address[] public funders;
 
     //defining constructor to define owner of contract immediately to msg.sender once contract is deployed
-    constructor 
+    constructor() public{
+        owner = msg.sender;
+    }
 
     function getfund() public payable {
         uint256 minimumUSD = 1 * 10 ** 18;
         require(getConversion(msg.value) >= minimumUSD, "You need to spend more ETH!");
+        //this mapping allows for the current sender address to map the amount being sent to their address
         addressToFund[msg.sender] = msg.value;
+        //this array will capture the address of the current sender of the fund into an array which will allow for resetting of the funds from the address later on once the withdraw function has been called
+        funders.push(msg.sender);
     }
 
     function getVersion() public view returns(uint256){
@@ -42,7 +49,7 @@ contract FundMe{
         return ethAmountInUSD;
     }//0.000002946735466460
 
-    //modifier is used to make a requirement check that msg.sender needs to be original owner who deployed contract before being alloweed to withdraw all the money
+    //modifier is used to make a requirement check that msg.sender needs to be original owner who deployed contract before being alloweed to withdraw all the money.  this can be used multiple times whenver it is called in a function.  eg can be used not only on withdraw function
     modifier onlyOwner {
         require(msg.sender == owner);
 
@@ -50,7 +57,14 @@ contract FundMe{
     }
 
     //this is a withdraw function which withdraws all the funded amount to the contract owners account address which is different from the contract address.  will need to add modifier to ensure that msg.sender needs to be the owner of the contract first before being able to withdraw the amount
-    function withdraw() public payable{
+    function withdraw() public payable onlyOwner{
         msg.sender.transfer(address(this).balance);
+        for (uint256 funderIndex=0; funderIndex < funders.length; funderIndex++){
+            //this is capturing the address of the funder from the funders array
+            address funder = funders[funderIndex];
+            //this is taking the mapping and using the funder address to reset the amount from the funder to zero
+            addressToFund[funder] = 0;
+        }
+        funders = new address[](0);
     }
 }
